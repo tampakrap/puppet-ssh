0f27faf4994189feb97f983d43a9d1ff2dc9b163
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-17 23:36:43 +0200

    Increase priority for Ports

--- manifests/config/server.pp
+++ manifests/config/server.pp
@@ -6,9 +6,12 @@ define ssh::config::server (
   include concat::setup
   include ssh::config
 
+  if $name == 'Ports' { $order = '01' }
+
   concat::fragment { $name:
     ensure   => $ensure,
     content  => template('ssh/config.erb'),
     target   => $ssh::server_config,
+    order    => $order
   }
 }
