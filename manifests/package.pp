c8f71dc03152c1d38d8af3930ccfc4911edcd76a
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-26 14:15:39 +0200

    $client_enabled is a local var

--- manifests/package.pp
+++ manifests/package.pp
@@ -9,12 +9,12 @@ class ssh::package {
   }
 
   if $ssh::client_package == 'absent' {
-    $ssh::client_enabled = false
+    $client_enabled = false
   } else {
-    $ssh::client_enabled = true
+    $client_enabled = true
   }
 
-  if $ssh::client_enabled and $ssh::client_package != $ssh::server_package {
+  if $client_enabled and $ssh::client_package != $ssh::server_package {
     package { $ssh::client_package:
       ensure => $ssh::client_ensure,
     }
